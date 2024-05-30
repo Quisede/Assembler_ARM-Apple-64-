a2ebc9b2dd9545959cb2e82f1dcfb3cd5cb2e80d
Quisede <daniil.druzhin129@yandex.com>
2024-05-30 23:18:23 +0300

    Create main.c

---
+++ LR_2_Assember /assembler_lr_2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  assembler_lr_2
+//
+//  Created by Даниил Дружинин on 22.03.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
